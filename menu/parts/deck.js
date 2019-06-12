15080c39534cbca4b92d4ee896c02b65d6b32a23
Sergio Peris <dev.sertxu@gmail.com>
2019-06-13 00:10:36 +0200

    Removed elements from the deck menu

--- menu/parts/deck.js
+++ menu/parts/deck.js
@@ -6,9 +6,6 @@ module.exports = () => {
     submenu: [
       { id: 'new', label: 'New...', click: deck.addNew },
       { type: 'separator' },
-      { id: 'insertBefore', label: 'Insert Before...' },
-      { id: 'insertAfter', label: 'Insert After...' },
-      { type: 'separator' },
       { id: 'duplicate', label: 'Duplicate' },
       { id: 'rename', label: 'Rename' },
       { type: 'separator' },
